14fbb1983816a7935376a0ead626b3762f1d629b
4166liz <32249159+4166liz@users.noreply.github.com>
2017-10-18 13:43:35 -0500

    Initial Commit

---
+++ geoMessager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  geoMessager
+//
+//  Created by sunny on 18/10/2017.
+//  Copyright © 2017 sunny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
